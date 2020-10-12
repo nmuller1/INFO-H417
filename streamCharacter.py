3cd36f658badb2c1e14e13a77ca4bc9179285b85
assou <astrid.soumoy@polymtl.ca>
2020-10-12 15:58:42 +0200

    AS: function tested

--- streamCharacter.py
+++ streamCharacter.py
@@ -1,25 +1,38 @@
 class StreamCharacter:
     """ Implement readln and writeln by reading / writing one character at a time. We continue reading characters (and collecting the characters read so far) until we read the end-of-line symbol """
-    def __init__(self):
+    def __init__(self, fileName):
+        self.filename = fileName
+        self.eof = False
         pass
 
     def open(self):
-        pass
+        self.file = open(self.filename, "r")
+        
 
-    def readIn(self):
-        pass
+    def readln(self):
+        char = " "
+        while char != "\n":
+            char = self.file.read(1)
+            print(char)
+            if not char:
+                self.eof = True
+                break
 
     def seek(self, pos):
-        pass
+        self.file.seek(pos)
 
     def end_of_stream(self):
-        pass
+        return self.eof
 
     def create(self):
-        pass
+        self.file = open(self.filename, "x")
 
-    def writeIn(self):
-        pass
+    def writeln(self, string):
+        for char in string:
+            self.file.write(char)
+        self.file.write("\n")
 
     def close(self):
-        pass
+        self.file.close()
+
+"""if __name__ == "__main__":"""
