b8aec48043693286fe775a742bddbf38e211ff35
assou <astrid.soumoy@polymtl.ca>
2020-10-21 01:22:01 +0200

    AS: start fourth algo

--- streamMapping.py
+++ streamMapping.py
@@ -1,25 +1,37 @@
+import mmap
+
 class StreamMapping:
     """ Read and write is performed by mapping and unmapping B characters of the file into internal memory through memory mapping. Whenever you need to read/write outside of the mapped portion, the next B element portion of the file is mapped. """
-    def __init__(self):
+    def __init__(self, fileName, buffer):
+        self.filename = filename
+        self.eof = False
         pass
 
     def open(self):
+        self.file = open(self.filename, "r")
         pass
 
     def readIn(self):
+        self.map = mmap.mmap(sel.file.fileno(), 0)
         pass
 
     def seek(self, pos):
+        self.map.seek(pos)
         pass
 
     def end_of_stream(self):
-        pass
+        return self.eof
 
     def create(self):
-        pass
+        self.file = open(self.filename, "x")
 
     def writeIn(self):
-        pass
+        for char in string:
+            self.map.write(char)
+        self.map.write("\n")
 
     def close(self):
-        pass
+        self.file.close()
+
+
+"""if __name__ == "__main__":"""
