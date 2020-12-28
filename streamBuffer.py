d9b512466e0ffcc0e968086ead24a4ac5adeba61
Muller Noëmie <noemie.muller@hotmail.com>
2020-12-28 13:11:08 +0100

    correction of StreamBuffer

--- streamBuffer.py
+++ streamBuffer.py
@@ -10,7 +10,7 @@ class StreamBuffer(Stream):
     def __init__(self, filename, bufferSize):
         Stream.__init__(self, filename)
         self.bufferSize = bufferSize
-        self.buffer = []
+        self.buffer = ""
 
     def readln(self):
         """
@@ -19,21 +19,12 @@ class StreamBuffer(Stream):
         """
         char = " "
         line = ""
-        while char and char != "\n":
-            while not self.bufferIsFull():
-                char = self.file.read(1)
-                if not char:
-                    self.eof = True
-                    break
-                if char == "\n":
-                    break
-                self.buffer.append(char)
-            for i in self.buffer:
-                line += i
-            self.cleanBuffer()
-        for i in self.buffer:
-            line += i
-        self.cleanBuffer()
+        while char != "\n":
+            self.buffer = self.file.read(self.bufferSize)
+            if len(self.buffer) < self.bufferSize: #end of file
+                self.eof = True
+                break
+            line += self.buffer
         return line
 
     def writeln(self, string):
@@ -44,13 +35,12 @@ class StreamBuffer(Stream):
         i = 0
         while i < len(string):
             while not self.bufferIsFull():
-                self.buffer.append(string[i])
+                self.buffer += string[i]
                 i += 1
                 if i == len(string):
                     break
-            for j in self.buffer:
-                self.file.write(j)
-            self.cleanBuffer()
+            self.file.write(self.buffer)
+            self.buffer = ""
         self.file.write("\n")
 
     def bufferIsFull(self):
@@ -59,16 +49,3 @@ class StreamBuffer(Stream):
         @return: True if the buffer is full and False if not
         """
         return len(self.buffer) == self.bufferSize
-
-    def bufferIsEmpty(self):
-        """
-        Checks if the buffer is empty
-        @return: True is the buffer is empty and False if not
-        """
-        return len(self.buffer) == 0
-
-    def cleanBuffer(self):
-        """
-        Empty the buffer
-        """
-        self.buffer = []
